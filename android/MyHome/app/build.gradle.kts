f1d9e985dd1191d992f87583d0c4aa562bff2cc8
Hui Yang <1191759350@qq.com>
2026-06-21 22:53:42 +0800

    commit

--- android/MyHome/app/build.gradle.kts
+++ android/MyHome/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 5
-        versionName = "0.0.6"
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- android/MyHome/app/src/main/java/com/yuwjoo/myhome/MainActivity.kt
+++ android/MyHome/app/src/main/java/com/yuwjoo/myhome/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        reconnectMqttIfNeeded()
+       reconnectMqttIfNeeded()
     }
 
     /**
