fb9dcf435f13daed7bcc9e7a5a8e56191741f382
YH <1191759350@qq.com>
2026-07-03 18:08:34 +0800

    commit

---
+++ web/my-home-mobile/src/modules/bridge/bridgeConfig.ts
@@ -0,0 +1,11 @@
+/**
+ * bridge配置
+ */
+
+export const bridgeConfig = {
+    nativeInterfaceKey: '__native-interface__',
+    commCenterKey: '__webBridge',
+    listenerGroup: '__listeners',
+    defaultTimeout: 60,
+    timeoutMessage: 'timeout',
+}
