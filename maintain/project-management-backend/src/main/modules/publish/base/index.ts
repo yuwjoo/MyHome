ef0c74db3840aae7c5e0f868124fbad903e3f20a
YH <1191759350@qq.com>
2026-09-14 18:08:26 +0800

    commit

---
+++ maintain/project-management-backend/src/main/modules/publish/base/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @file 发布控制器基类出口
+ * @description 对外暴露发布流程基类，供各项目类型的发布控制器继承
+ */
+export { BasePublishController } from './basePublishController'
--- maintain/project-management-backend/src/main/modules/publish/index.ts
+++ maintain/project-management-backend/src/main/modules/publish/index.ts
@@ -2,6 +2,7 @@
  * @file 发布模块出口
  * @description 对外暴露发布相关能力：凭据读取、OSS 客户端、Shell 命令执行
  */
+export { BasePublishController } from './base'
 export { getOssClient } from './modules/oss'
 export { fetchCredentials, refreshCredentials } from './modules/secret'
 export type { ICredentials, IOssConfig } from './modules/secret'
