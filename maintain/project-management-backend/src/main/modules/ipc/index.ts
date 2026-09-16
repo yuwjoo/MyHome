c2b8f6ea07e72ea9623969f641315178649b530a
YH <1191759350@qq.com>
2026-09-16 17:20:41 +0800

    commit

--- maintain/project-management-backend/src/main/modules/ipc/index.ts
+++ maintain/project-management-backend/src/main/modules/ipc/index.ts
@@ -3,7 +3,7 @@
  * @description 注册各域 IPC 通道，并对外提供主进程向渲染进程推送消息的能力
  */
 import { send } from './utils/handler'
-import './modules/release'
+import './handlers/release'
 
 // 主进程向渲染进程推送消息
 export const ipcSend = send
